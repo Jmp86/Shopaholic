5c112001dd72984b83f7bb418c9b4aa9ac17f85a
Justin <jmp86@yahoo.com>
2022-07-24 01:50:33 -0500

    added a few pictures to SBC

--- client/src/components/CategoryCard.js
+++ client/src/components/CategoryCard.js
@@ -8,46 +8,46 @@ import InfoIcon from '@mui/icons-material/Info';
 
 const itemData = [
     {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      img: 'https://media.istockphoto.com/photos/set-of-contemporary-house-appliances-isolated-on-white-picture-id1174598609?k=20&m=1174598609&s=612x612&w=0&h=c5rP2tqv0Uck6zm21hBYhEyVy9EBAA7C5VGI4sgfmzo=',
       title: 'Appliances',
       rows: 2,
       cols: 2,
     },
     {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'Clothing',
+      img: 'https://i5.walmartimages.com/asr/89423986-7678-471d-9b08-f7aa4569f6f3.27b8f54d62fc40f47b5e6b1cea4f4d1a.jpeg',
+      title: "TV's",
     },
     {
       img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Electronics',
+      title: "Men's Clothing",
     },
     {
       img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-      title: 'Coffee',
+      title: "Women's Clothing",
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-      title: 'Hats',
+      title: 'Jewelry',
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-      title: 'Honey',
+      title: 'Gaming',
       rows: 2,
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-      title: 'Basketball',
+      title: 'Phones',
     },
     {
       img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-      title: 'Fern',
+      title: 'Tablets',
     },
     {
       img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-      title: 'Mushrooms',
+      title: 'Sports Equipment',
       rows: 2,
       cols: 2,
     }
@@ -62,8 +62,8 @@ const CategoryCard = ({category}) => {
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
             <img
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img}`}
+              srcSet={`${item.img}`}
               alt={item.title}
               loading="lazy"
             />
@@ -75,7 +75,7 @@ const CategoryCard = ({category}) => {
                   sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
                   aria-label={`info about ${item.title}`}
                 >
-                  <InfoIcon />
+                
                 </IconButton>
               }
             />
