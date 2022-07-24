b952a5dabe50412227bf10ae2fb08645809b6c78
Justin <jmp86@yahoo.com>
2022-07-24 01:41:47 -0500

    changed shopy by category layout

--- client/src/components/CategoryCard.js
+++ client/src/components/CategoryCard.js
@@ -1,42 +1,88 @@
 import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
 
+const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Appliances',
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Clothing',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Electronics',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Coffee',
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+      title: 'Hats',
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+      title: 'Honey',
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+      title: 'Basketball',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+      title: 'Fern',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+      title: 'Mushrooms',
+      rows: 2,
+      cols: 2,
+    }
+  ];
 
 const CategoryCard = ({category}) => {
 
     return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-          </ListItemAvatar>
-          <ListItemText
-            primary={
-              <React.Fragment>
-                <Typography
-                  sx={{ display: 'inline' }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
+        <ImageList sx={{ width: 'auto', height: 800 }}>
+        <ImageListItem key="Subheader" cols={2}>
+        </ImageListItem>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={item.title}
+              subtitle={item.author}
+              actionIcon={
+                <IconButton
+                  sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                  aria-label={`info about ${item.title}`}
                 >
-                  {category.name}
-                </Typography>
-                {/* {" — I'll be in your neighborhood doing errands this…"} */}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-      </List>
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     );
-  
-
-  
-}
+  }
 
 export default CategoryCard;
--- client/src/containers/ShopByCategory.js
+++ client/src/containers/ShopByCategory.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import CategoryCard from '../components/CategoryCard';
-import {ItemContext} from '../context/item';
-import {useContext, useState, useEffect, useCallback} from 'react';
-import {MessageContext} from "../context/message";
-import {categories} from '../config';
+// import {ItemContext} from '../context/item';
+// import {useContext, useState, useEffect, useCallback} from 'react';
+// import {MessageContext} from "../context/message";
+// import {categories} from '../config';
 
 const ShopByCategory = () => {
     // const {getCategories} = useContext(ItemContext);
-    const [apiCategories, setapiCategories] = useState([]);
-    const {setMessage} = useContext(MessageContext)
+    // const [apiCategories, setapiCategories] = useState([]);
+    // const {setMessage} = useContext(MessageContext)
 
-    useEffect(() => { 
-            fetch('https://amazon24.p.rapidapi.com/api/category?country=US', categories)
-            .then(r => r.json())
-            .then(setapiCategories)
-        // } catch (e) {
-        //     setMessage({message: "No items to display", color: "red"})
-        // }
-    }, [])
-    console.log(apiCategories)
     return (
         <div>
-            <h1>Select a category to begin shopping</h1>
-            {apiCategories ? apiCategories.map(category => <CategoryCard key={category.id} category={category}/>) : null}
+            <h2>Select a category to begin shopping</h2>
+            <CategoryCard/>
             {/* {post.reviews ? post.reviews.map(review => <ReviewTile key={review.id} review={review} deleteReview={deleteReview}/>) : null} */}
         </div>
     );
